7bb0442c2e6614ca61059a27fd3a5fb72b79d555
Michal <mkuc@ipp.mpcdf.mpg.de>
2025-03-30 08:20:49 +0200

    trying

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/bar-ze-smakiem',
-  assetPrefix: '/bar-ze-smakiem',
   images: {
     unoptimized: true,
   },
